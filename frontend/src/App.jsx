1af0ebe96dafdff73d8aedac6054edabe1823027
Leugard <kirakazama01@gmail.com>
2025-05-17 09:46:18 +0700

    feat: initialize frontend structure with routing, authentication, and onboarding features
    - Added main application structure with routing using React Router.
    - Implemented authentication hooks to manage user state.
    - Created pages for Home, Sign Up, Login, Notifications, Call, Chat, and Onboarding.
    - Integrated API calls for user signup and authentication.
    - Added a loading component for better user experience during data fetching.
    - Established theme constants for UI customization.
    - Configured Tailwind CSS with DaisyUI for styling.
    - Included SVG logo for branding.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,81 @@
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/Homepage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/Loginpage";
+import NotificationsPage from "./pages/NotificationsPage";
+import CallPage from "./pages/CallPage";
+import ChatPage from "./pages/ChatPage";
+import OnboardingPage from "./pages/OnboardingPage";
+import { Toaster } from "react-hot-toast";
+import PageLoader from "./components/PageLoader";
+import useAuthUser from "./hooks/useAuthUser";
+
+const App = () => {
+  const { isLoading, authUser } = useAuthUser();
+
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded;
+
+  if (isLoading) return <PageLoader />;
+
+  return (
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/signup" : "/onboarding"} />
+            )
+          }
+        />
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/notifications"
+          element={
+            isAuthenticated ? (
+              <NotificationsPage />
+            ) : (
+              <Navigate to={"/signup"} />
+            )
+          }
+        />
+        <Route
+          path="/call"
+          element={isAuthenticated ? <CallPage /> : <Navigate to={"/signup"} />}
+        />
+        <Route
+          path="/chat"
+          element={isAuthenticated ? <ChatPage /> : <Navigate to={"/signup"} />}
+        />
+        <Route
+          path="/onboarding"
+          element={
+            isAuthenticated ? (
+              !isOnboarded ? (
+                <OnboardingPage />
+              ) : (
+                <Navigate to={"/"} />
+              )
+            ) : (
+              <Navigate to={"/signup"} />
+            )
+          }
+        />
+      </Routes>
+
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
